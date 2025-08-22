0fae0a491279a1ff970732fec04b734e7fd24a7e
EtecVAV-C2-Bolacha <bolacha.etecvav@gmail.com>
2025-08-22 18:14:08 -0300

    Update atualizacao_produtos.php 22-08-2025

--- atualizacao_produtos.php
+++ atualizacao_produtos.php
@@ -76,8 +76,8 @@
         
         button { 
             padding: 10px 15px;
-            background:rgb(19, 10, 100);
-            color: white; 
+            background-color:rgba(255, 170, 121, 1);
+            color: black; 
             font-size: 15px;
             border-radius: 10px; 
             cursor: pointer; 
@@ -94,4 +94,5 @@
     </div>
 </body>
 </html>
+
 
