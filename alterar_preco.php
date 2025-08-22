132d5e4a93e227bed8909ab6b1a04aa45cf02e3f
EtecVAV-C2-Bolacha <bolacha.etecvav@gmail.com>
2025-08-22 18:13:37 -0300

    Update alterar_preco.php 22-08-2025

--- alterar_preco.php
+++ alterar_preco.php
@@ -81,8 +81,8 @@
         }
         button { 
             padding: 10px 15px;
-            background:rgb(19, 10, 100);
-            color: white; 
+            background-color:rgba(255, 170, 121, 1);
+            color: black; 
             font-size: 15px;
             border-radius: 10px; 
             cursor: pointer; 
@@ -124,5 +124,6 @@
     </div>
 </body>
 </html>
+
 
 
